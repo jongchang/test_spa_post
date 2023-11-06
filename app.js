8bab013dcf9be74adb6b829123ed5b6b7ee28336
jongchang <hokhmah91@naver.com>
2023-11-06 21:02:16 +0900

    commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = 3018;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/api', [usersRouter, postsRouter]);
 
